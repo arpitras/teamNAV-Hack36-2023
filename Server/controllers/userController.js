980d08d596312833b537328870f8bcd78fa950ba
Arpit Rastogi <arpitkrastogi23@gmail.com>
2023-03-26 00:02:41 +0530

    by arpit

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -1,4 +1,17 @@
+const User = require("../models/userModel")
 
+
+const loadHome = (req,res) =>{
+    try {
+        res.render("home");
+    } catch (error) {
+        console.log(error.message);
+    }
+};
+
+module.exports = {
+    loadHome
+};
 module.exports = {
     loadHome
 };
--- Server/middleware/auth.js
+++ Server/middleware/auth.js
@@ -4,11 +4,7 @@ const isLogin = async(req,res,next)=>{
 
         }
         else{
-<<<<<<< HEAD
             res.redirect("http://localhost:3030/login");
-=======
-            res.redirect("/login");
->>>>>>> 6a7eb66a7d91842f10c46e0e87c2e3d6cad983c9
         }
         next();
     } catch (error) {
@@ -18,11 +14,7 @@ const isLogin = async(req,res,next)=>{
 const isLogout = async(req,res,next)=>{
     try {
         if(req.session.user){
-<<<<<<< HEAD
             res.redirect("http://localhost:3000");
-=======
-            res.redirect("/join_N");
->>>>>>> 6a7eb66a7d91842f10c46e0e87c2e3d6cad983c9
         }
         next();
     } catch (error) {
