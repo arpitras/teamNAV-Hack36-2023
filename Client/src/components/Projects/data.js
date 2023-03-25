08f54c062e1f0f544c16a450786eef1e796f1605
Vinayak Sharma <sharmavinayak0206@gmail.com>
2023-03-26 00:10:29 +0530

    by vinayak

---
+++ Client/src/components/Projects/data.js
@@ -0,0 +1,47 @@
+const projectArray = [
+    {
+        id:1,
+        projectTitle:'',
+        projectStack:'React | Typescript',
+        projectInfo: '',
+        imageRedirect : "https://mail.google.com/mail/u/0/#inbox",
+        pathToImage : "https://www.shutterstock.com/image-vector/lets-play-neon-sign-bright-600w-1310883713.jpg",
+        upper : "title",
+    },
+    {
+        id:2,
+        projectTitle:'Motivational quotes for you',
+        projectStack:'React | Typescript',
+        projectInfo: 'Can give you new feelings of inspiration, and affirmative uplift',
+        imageRedirect : "https://parade.com/1037762/kimberlyzapata/mental-health-quotes/",
+        pathToImage : "https://media.istockphoto.com/id/1264156957/photo/young-man-in-the-city-reaching-his-goals.jpg?s=612x612&w=0&k=20&c=-xtXMjc8CecrJWLkETw6yk6-yVhswh7w1_uh5ele820=",
+    },
+    {
+        id:3,
+        projectTitle:'Control anger before it controls you...',
+        projectStack:'React | Typescript',
+        projectInfo: 'Is your temper hijacking your life? These tips and techniques can help you get anger under control and express your feelings in healthier ways',
+        pathToImage : "https://thepsychologygroup.com/wp-content/uploads/2020/02/Panic-Attacks-Signs-and-Symptoms.jpg",
+        imageRedirect: "https://www.helpguide.org/articles/relationships-communication/anger-management.htm"
+    },
+    {
+        id:4,
+        projectTitle:'',
+        projectStack:'React | Typescript',
+        projectInfo: '',
+        imageRedirect : "",
+        pathToImage : "https://static.theprint.in/wp-content/uploads/2020/08/studying.jpg?compress=true&quality=80&w=376&dpr=2.6",
+        
+    },
+    {
+        id:5,
+        projectTitle:'',
+        projectStack:'React | Typescript',
+        projectInfo: '',
+        imageRedirect : "",
+        pathToImage : "https://img.freepik.com/free-vector/organic-flat-people-meditating-illustration_23-2148906556.jpg?w=2000",
+    }
+
+]
+
+export default projectArray
