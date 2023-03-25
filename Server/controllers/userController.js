daad7d0b9cdac4619c2097860f186f378a5fbd13
Arpit Rastogi <arpitkrastogi23@gmail.com>
2023-03-25 14:26:12 +0530

    by arpit

---
+++ Server/controllers/userController.js
@@ -0,0 +1,14 @@
+const User = require("../models/userModel")
+
+
+const loadHome = (req,res) =>{
+    try {
+        res.render("home");
+    } catch (error) {
+        console.log(error.message);
+    }
+};
+
+module.exports = {
+    loadHome
+};
---
+++ Server/models/userModel.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = mongoose.Schema({
+    username:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    is_verified:{
+        type:Number,
+        default:0
+    },
+    token:{
+        type:String,
+        default:''
+    }
+})
+
+const User = mongoose.model("User",userSchema);
+
+module.exports = User;
